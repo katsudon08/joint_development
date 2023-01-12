05bc8400465450bfe727beb51ffecc72eea5ebd2
matsu <matsuaki171101@gmail.com>
2023-01-13 08:39:34 +0900

    くそコードを動かせるように修正

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 #include <windows.h>
 // exit()関数を使用するための外部ライブラリ
 #include <stdlib.h>
+#include <string.h>
 
 void keyboard_input(int, int, int *);
 void display_transition(int);
@@ -14,7 +15,7 @@ void display_transition(int);
 
 int main(void) {
     // 現在の選択画面を判別するための識別コード
-    int currentStateNum = 4;
+    int currentStateNum = 0;
 
     // 文字コードをutf8に変更するシェルコマンド
     system("chcp 65001");
@@ -47,16 +48,16 @@ void keyboard_input(int getNumOnce, int getNumTwice, int *currentStateNum) {
                 case 0x48:
                     system("cls");
                     if(0<*currentStateNum && *currentStateNum<4) {
-                        // ()をつけないと*演算子よりもインクリメントが優先され、アドレスをインクリメントすることになってしまうため、()を付ける。
-                        (*currentStateNum)++;
+                        // ()をつけないと*演算子よりもインクリメントが優先され、アドレスをデクリメントすることになってしまうため、()を付ける。
+                        (*currentStateNum)--;
                     }
                     display_transition(*currentStateNum);
                     break;
                 // ↓キーが押された時
                 case 0x50:
                     system("cls");
-                    if(1<*currentStateNum && *currentStateNum<5) {
-                        (*currentStateNum)--;
+                    if(-1<*currentStateNum && *currentStateNum<3) {
+                        (*currentStateNum)++;
                     }
                     display_transition(*currentStateNum);
                     break;
@@ -93,31 +94,12 @@ void keyboard_input(int getNumOnce, int getNumTwice, int *currentStateNum) {
 void display_transition(int currentStateNum) {
     char *normalState[4] = {"  入力\n", "  検索\n", "  メダル総獲得順一覧\n", "  国名順一覧\n"};
     char *selectState[4];
+    char allow[50] = "➤";
+    strcat(allow, normalState[currentStateNum]);
 
-    switch(currentStateNum) {
-        case 1:
-            for(int i = 0; i < 4; i++) {
-                selectState[i] = normalState[i];
-            }
-            selectState[4] = "➤%s", normalState[4];
-            break;
-        case 2:
-            for(int i = 0; i < 4; i++) {
-                selectState[i] = normalState[i];
-            }
-            selectState[3] = "➤%s", normalState[3];
-            break;
-        case 3:
-            for(int i = 0; i < 4; i++) {
-                selectState[i] = normalState[i];
-            }
-            selectState[2] = "➤%s", normalState[2];
-            break;
-        case 4:
-            for(int i = 0; i < 4; i++) {
-                selectState[i] = normalState[i];
-            }
-            selectState[1] = "➤%s", normalState[1];
-            break;
+    for(int i = 0; i < 4; i++) {
+        selectState[i] = normalState[i];
+        selectState[currentStateNum] = allow;
+        printf("%s", selectState[i]);
     }
 }
