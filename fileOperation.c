a0a23a8eb090c39ec05340261e1af5e235c5e02c
matsu <matsuaki171101@gmail.com>
2023-01-13 11:00:27 +0900

    ファイル操作の関数の枠組み作成

---
+++ fileOperation.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define BUF_SIZE 256 // 一行の最大文字数(バイト数)
+
+typedef struct {
+    char countryName[BUF_SIZE];
+    int medalRank;
+    int medalGetNum;
+} object; //オブジェクト型して、定義
+
+int main(void) {
+    char *modeIdentify = "r";
+
+    // FILE型構造体
+    FILE *fp;
+
+    // ファイル操作を行うファイル名
+    char fname[] = "data.csv";
+
+    // fgets関数や、sscanf関数を用いる際に使用する一行分の文字列を格納する変数
+    char lineString[BUF_SIZE];
+    // 検索した文字列を格納する変数
+    char searchCountryName[BUF_SIZE];
+
+    // 検索した国が存在するかどうかの識別変数
+    int isExist = 0;
+
+    // 構造体をインスタンス化
+    object data;
+
+    // ファイルを開き、読み込む
+    fp = fopen(fname, modeIdentify);
+
+    switch(modeIdentify[0]) {
+        case 'r':
+            if(fp!=NULL) {
+                // 第一引数:一行分の文字列を格納する配列
+                // 第二引数:一行の最大文字数
+                // 第三引数:FILE型構造体の実体のアドレス
+                // fgets関数で読み込みに失敗するとnullを返す。
+                while(fgets(lineString, BUF_SIZE, fp) != NULL) {
+                    // 第一引数にファイル操作の対象の変数
+                    // 第二引数に読み込まれた書式指定子
+                    // 第三引数にファイルで読み込んだ文字列を格納する変数
+                    sscanf(lineString, "%s %d %d", data.countryName, &data.medalRank, &data.medalGetNum);
+                    // 第一引数の変数と第二引数の変数の文字列を比較して、一致すれば0を返す。
+                    if(strcmp(searchCountryName, data.countryName)==0) {
+                        // コマンドプロントの履歴を消去
+                        system("cls");
+                        printf("======================================\n");
+                        printf("\t国名\t\t:%s\n", data.countryName);
+                        printf("\tメダル順位\t:%d\n", data.medalRank);
+                        printf("\t獲得メダル数\t:%d\n", data.medalGetNum);
+                        printf("======================================\n");
+                        isExist++;
+                        break;
+                    }
+                }
+            }else { // ファイルが存在しなかった場合のエラー
+                printf("Error: 読み込むファイルが見つかりませんでした。");
+                exit(0);
+            }
+
+            // 検索した国がファイル内に存在しなかった場合のエラー
+            if(isExist == 0) {
+                printf("Error: 検索した条件と一致する国が見つかりませんでした。");
+                exit(0);
+            }
+            break;
+        case 'w':
+            if(fp!=NULL) {
+                // ---------------------------------
+                // ファイルの書き込み処理の適応をすること
+                // ---------------------------------
+                int result = fprintf(fp, "a");
+
+                if(result == -1) {
+                    printf("Error: ファイルの書き込みに失敗しました。");
+                    exit(0);
+                }
+            }else {
+                printf("Error: 書き込むファイルが見つかりませんでした。");
+                exit(0);
+            }
+            break;
+    }
+
+    // ファイル操作を行った後は必ずファイルを閉じる必要がある。
+    fclose(fp);
+
+    return 0;
+}
---
+++ writeFile.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void) {
+    FILE *fp;
+
+    return 0;
+}
